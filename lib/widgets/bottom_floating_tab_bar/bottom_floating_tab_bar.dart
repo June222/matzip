2e2576a979cb85d1bc90cd94dea092405968a2f6
June222 <38900003+June222@users.noreply.github.com>
2023-02-09 14:54:47 +0900

    feat: bottomNavBar, png파일로 이미지 대체 및 버튼생성

--- lib/widgets/bottom_floating_tab_bar/bottom_floating_tab_bar.dart
+++ lib/widgets/bottom_floating_tab_bar/bottom_floating_tab_bar.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import 'bottom_floating_tab_button.dart';
 
 class BottomFloatingTabBar extends StatelessWidget {
   const BottomFloatingTabBar({
@@ -13,55 +10,66 @@ class BottomFloatingTabBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(2.5),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        padding: !_bottomAppear
-            ? const EdgeInsets.symmetric(vertical: 5)
-            : EdgeInsets.zero,
-        height: 40,
-        width: !_bottomAppear ? MediaQuery.of(context).size.width * 0.9 : 0,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Colors.black.withOpacity(0.6),
-            Colors.black.withOpacity(0.8)
-          ]),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Positioned(
-              left: 40,
-              child: BottomFloatingTabBarButton(
-                icon: FontAwesomeIcons.fileArrowUp,
-                bottomAppear: _bottomAppear,
-              ),
-            ),
-            Positioned(
-              left: 120,
-              child: BottomFloatingTabBarButton(
-                icon: FontAwesomeIcons.book,
-                bottomAppear: _bottomAppear,
-              ),
-            ),
-            Positioned(
-              right: 120,
-              child: BottomFloatingTabBarButton(
-                icon: FontAwesomeIcons.listCheck,
-                bottomAppear: _bottomAppear,
-              ),
-            ),
-            Positioned(
-              right: 40,
-              child: BottomFloatingTabBarButton(
-                icon: FontAwesomeIcons.gear,
-                bottomAppear: _bottomAppear,
-              ),
-            ),
-          ],
-        ),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: 50,
+      width: !_bottomAppear ? MediaQuery.of(context).size.width * 0.92 : 0,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Colors.black.withOpacity(0.6),
+          Colors.black.withOpacity(0.8)
+        ]),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          /// 이미지로 실행하였으나 애니매이션 적용 안됨
+          Positioned(
+              left: 20,
+              bottom: 0,
+              child: IconButton(
+                icon: Image.asset(
+                  "assets/images/write7.png",
+                  height: 35,
+                ),
+                padding: EdgeInsets.zero,
+                onPressed: () {},
+              )),
+          Positioned(
+              left: 110,
+              bottom: 0,
+              child: IconButton(
+                icon: Image.asset(
+                  "assets/images/write7.png",
+                  height: 35,
+                ),
+                padding: EdgeInsets.zero,
+                onPressed: () {},
+              )),
+          Positioned(
+              right: 110,
+              bottom: 0,
+              child: IconButton(
+                icon: Image.asset(
+                  "assets/images/write7.png",
+                  height: 35,
+                ),
+                padding: EdgeInsets.zero,
+                onPressed: () {},
+              )),
+          Positioned(
+              right: 20,
+              bottom: 0,
+              child: IconButton(
+                icon: Image.asset(
+                  "assets/images/write7.png",
+                  height: 35,
+                ),
+                padding: EdgeInsets.zero,
+                onPressed: () {},
+              )),
+        ],
       ),
     );
   }
