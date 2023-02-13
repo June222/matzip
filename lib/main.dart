247d4483c735e1ed8704d1d3247556f75bc229e5
June222 <38900003+June222@users.noreply.github.com>
2023-02-13 14:19:14 +0900

    firestore 사용 자동 추가된 부분

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'package:busan_univ_matzip/firebase_options.dart';
 import 'package:busan_univ_matzip/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
-import 'firebase_options.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   // Bloc.observer = AppBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // await FirebaseAuth.instance.useAuthEmulator('localhost', 5033);
+  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   await SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp]); // 회전 금지
+    [DeviceOrientation.portraitUp],
+  ); // 회전 금지
 
   runApp(const MyApp());
 }
