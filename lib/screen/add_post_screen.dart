e4e576ab7d64abd46d8fe1bf26fc421c3f27a991
June222 <38900003+June222@users.noreply.github.com>
2023-02-15 19:08:59 +0900

    refactor: post_text_field 분리

--- lib/screen/add_post_screen.dart
+++ lib/screen/add_post_screen.dart
@@ -3,6 +3,7 @@ import 'package:busan_univ_matzip/managers/image_manager.dart';
 import 'package:busan_univ_matzip/providers/user_provider.dart';
 import 'package:busan_univ_matzip/resources/firestore_method.dart';
 import 'package:busan_univ_matzip/widgets/custom_indicator.dart';
+import 'package:busan_univ_matzip/widgets/post_screen_text_field.dart';
 import 'package:busan_univ_matzip/widgets/snackbar.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -142,61 +143,6 @@ class _AddPostScreenState extends State<AddPostScreen> {
                         ),
                     child: const Text("게시"))
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class PostScreenTextField extends StatelessWidget {
-  const PostScreenTextField({
-    super.key,
-    required TextEditingController typeController,
-    required String labelText,
-    this.expands = false,
-    this.suffixText = "",
-    this.minLines = 1,
-    this.maxLines = 1,
-  })  : _labelText = labelText,
-        _typeController = typeController;
-
-  final TextEditingController _typeController;
-  final String _labelText;
-  final bool expands;
-  final String suffixText;
-  final int minLines;
-  final int maxLines;
-
-  @override
-  Widget build(BuildContext context) {
-    // ignore: no_leading_underscores_for_local_identifiers
-    int? _maxLines = maxLines;
-    // ignore: no_leading_underscores_for_local_identifiers
-    int? _minLines = minLines;
-    if (expands) {
-      _maxLines = null;
-      _minLines = null;
-    }
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Scrollbar(
-        radius: Radius.zero,
-        child: TextField(
-          controller: _typeController,
-          textInputAction: TextInputAction.newline,
-          expands: expands,
-          maxLines: _maxLines,
-          minLines: _minLines,
-          onTapOutside: (event) => FocusScope.of(context).unfocus(),
-          decoration: InputDecoration(
-            labelText: _labelText,
-            labelStyle: const TextStyle(fontSize: 14),
-            suffixText: suffixText,
-            focusedBorder: const UnderlineInputBorder(
-              borderSide: BorderSide.none,
-            ),
-          ),
         ),
       ),
     );
