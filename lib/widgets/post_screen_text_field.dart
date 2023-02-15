338c3b8ea2b0ba369c73032983682f7ac1f5de78
June222 <38900003+June222@users.noreply.github.com>
2023-02-15 19:33:09 +0900

    refactor: PostTextField 분리

---
+++ lib/widgets/post_screen_text_field.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class PostScreenTextField extends StatelessWidget {
+  const PostScreenTextField({
+    super.key,
+    required TextEditingController typeController,
+    required String labelText,
+    this.expands = false,
+    this.suffixText = "",
+    this.minLines = 1,
+    this.maxLines = 1,
+  })  : _labelText = labelText,
+        _typeController = typeController;
+
+  final TextEditingController _typeController;
+  final String _labelText;
+  final bool expands;
+  final String suffixText;
+  final int minLines;
+  final int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: no_leading_underscores_for_local_identifiers
+    int? _maxLines = maxLines;
+    // ignore: no_leading_underscores_for_local_identifiers
+    int? _minLines = minLines;
+    if (expands) {
+      _maxLines = null;
+      _minLines = null;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Scrollbar(
+        radius: Radius.zero,
+        child: TextField(
+          controller: _typeController,
+          textInputAction: TextInputAction.newline,
+          expands: expands,
+          maxLines: _maxLines,
+          minLines: _minLines,
+          onTapOutside: (event) => FocusScope.of(context).unfocus(),
+          decoration: InputDecoration(
+            labelText: _labelText,
+            labelStyle: const TextStyle(fontSize: 14),
+            suffixText: suffixText,
+            focusedBorder: const UnderlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
