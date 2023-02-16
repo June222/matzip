f8279da8313a4b9a436a2ed9432444c2855004bd
June222 <38900003+June222@users.noreply.github.com>
2023-02-16 16:10:35 +0900

    style: 상단-연하게, 하단-애니메이션으로 진하게

--- lib/widgets/add_post_button.dart
+++ lib/widgets/add_post_button.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class AddPostButton extends StatelessWidget {
+class AddPostButton extends StatefulWidget {
   const AddPostButton({
     super.key,
     required this.onPressed,
@@ -9,17 +10,35 @@ class AddPostButton extends StatelessWidget {
   });
   final Function() onPressed;
   final bool offstage;
+
+  @override
+  State<AddPostButton> createState() => _AddPostButtonState();
+}
+
+class _AddPostButtonState extends State<AddPostButton>
+    with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Offstage(
-      offstage: offstage,
-      child: FloatingActionButton(
-        onPressed: onPressed,
-        child: const FaIcon(
-          FontAwesomeIcons.penToSquare,
-          size: 30,
-        ),
+    return FloatingActionButton(
+      onPressed: widget.onPressed,
+      tooltip: "게시글 작성",
+      child: const FaIcon(
+        FontAwesomeIcons.penToSquare,
+        size: 30,
       ),
+    ).animate(
+      onPlay: (controller) {
+        if (widget.offstage) {
+          controller.reverse();
+        } else {
+          controller.forward();
+        }
+      },
+    ).fade(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.decelerate,
+      begin: 0.3,
+      end: 1.0,
     );
   }
 }
