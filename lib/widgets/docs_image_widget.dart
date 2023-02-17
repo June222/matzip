45fb3975200ccb261d2d0c023e0e0885a8fe2c45
June222 <38900003+June222@users.noreply.github.com>
2023-02-17 13:32:10 +0900

    style: docsImageWidget padding 제거

--- lib/widgets/docs_image_widget.dart
+++ lib/widgets/docs_image_widget.dart
@@ -15,24 +15,21 @@ class DocsImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 5),
-      child: Image.network(
-        docs['postURL'].toString(),
-        width: width,
-        height: height,
-        fit: BoxFit.cover,
-        frameBuilder: (context, child, frame, wasSynchronouslyLoaded) => child,
-        loadingBuilder: (context, child, loadingProgress) {
-          if (loadingProgress == null) {
-            return child;
-          } else {
-            return const CustomIndicator(offstage: false);
-          }
-        },
-        errorBuilder: (context, error, stackTrace) =>
-            const Text("no Image to show"),
-      ),
+    return Image.network(
+      docs['postURL'].toString(),
+      width: width,
+      height: height,
+      fit: BoxFit.cover,
+      frameBuilder: (context, child, frame, wasSynchronouslyLoaded) => child,
+      loadingBuilder: (context, child, loadingProgress) {
+        if (loadingProgress == null) {
+          return child;
+        } else {
+          return const CustomIndicator(offstage: false);
+        }
+      },
+      errorBuilder: (context, error, stackTrace) =>
+          const Text("no Image to show"),
     );
   }
 }
