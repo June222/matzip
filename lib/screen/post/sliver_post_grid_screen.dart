8380f3f30c41ea32ded286d258376c70d770d0fa
June222 <38900003+June222@users.noreply.github.com>
2023-03-11 15:50:14 +0900

    fix: image overflow 해결

--- lib/screen/post/sliver_post_grid_screen.dart
+++ lib/screen/post/sliver_post_grid_screen.dart
@@ -1,3 +1,7 @@
+// import 'dart:math';
+
+import 'dart:developer';
+
 import 'package:busan_univ_matzip/widgets/post/small_post_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -12,11 +16,13 @@ class SliverPostGridScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    log(size.toString());
     return SliverGrid.builder(
       itemCount: querySnapShot.docs.length,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        childAspectRatio: 10 / 13,
+        childAspectRatio: 8 / 13,
       ),
       itemBuilder: (_, index) => SmallPostWidget(
         docs: querySnapShot.docs[index].data(),
